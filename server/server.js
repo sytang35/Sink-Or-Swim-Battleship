443b9a81e6392405ac1e7c5c9076eb5a7f8d9c26
Sam <samywtang@gmail.com>
2020-09-19 18:13:02 -0400

    Set up test socket.io server for players

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const singlePlayer = require("./singlePlayer");
 app.use(express.json());
 app.use(cors());
 
+let players = [null, null];
+io.on("connect", (socket) => {
+  // Test socket connection
+  let playerIndex = 0;
+  for (let i in players) {
+    if (players[i] === null) {
+      playerIndex = i + 1;
+    }
+  }
+  console.log(`Player ${playerIndex} connected`);
+
+  socket.emit("Player-number", playerIndex);
+
+  if (playerIndex === 0) return;
+  players[playerIndex] = socket;
+
+  socket.broadcast.emit("Player-connect", playerIndex);
+});
 // Single player server data
 
 app.use("/", singlePlayer);
